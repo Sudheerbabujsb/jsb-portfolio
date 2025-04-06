e6355707fddee61fcb7afeec23e0aebfcabb7e33
Jangam Sudheer Babu <sudheerbabujsb@gmail.com>
2025-04-07 00:39:56 +0530

    jsb-portfolio

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,15 +1,15 @@
-// contact.js
 document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-  
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const subject = document.getElementById("subject").value.trim();
-    const message = document.getElementById("comment").value.trim();
-  
-    console.log({ name, email, subject, message });
-    alert("I'll Reach you Soon - JSB");
-  
-    this.reset();
-  });
-  
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const subject = document.getElementById("subject").value.trim();
+  const message = document.getElementById("comment").value.trim();
+
+  // You can send this data to a server or service here
+  console.log({ name, email, subject, message });
+
+  // alert("I'll Reach you Soon - JSB");
+
+  this.reset();
+});

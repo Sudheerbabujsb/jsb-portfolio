69bd904fbca24bc5d450ef5b0b09f5e813fb29b7
Jangam Sudheer Babu <sudheerbabujsb@gmail.com>
2025-06-04 20:59:38 +0530

    removed profile

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("contactForm");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const form = document.getElementById("contactForm");
 
-  form.addEventListener("submit", async function (e) {
-    e.preventDefault();
+//   form.addEventListener("submit", async function (e) {
+//     e.preventDefault();
 
-    const formData = new FormData(form);
+//     const formData = new FormData(form);
 
-    try {
-      const response = await fetch("https://formspree.io/f/movepjng", {
-        method: "POST",
-        body: formData,
-      });
+//     try {
+//       const response = await fetch("https://formspree.io/f/movepjng", {
+//         method: "POST",
+//         body: formData,
+//       });
 
-      console.log("Response status:", response.status);
+//       console.log("Response status:", response.status);
 
-      if (response.ok) {
-        console.log("✅ Form submitted successfully. Redirecting...");
-        window.location.href = "thank-you.html";
-      } else {
-        const errorText = await response.text();
-        console.error("❌ Form submission failed:", errorText);
-        alert("Something went wrong. Please try again.");
-      }
-    } catch (error) {
-      console.error("⚠️ Fetch error:", error);
-      alert("Error submitting form. Check console for more details.");
-    }
-  });
-});
+//       if (response.ok) {
+//         console.log("✅ Form submitted successfully. Redirecting...");
+//         window.location.href = "thank-you.html";
+//       } else {
+//         const errorText = await response.text();
+//         console.error("❌ Form submission failed:", errorText);
+//         alert("Something went wrong. Please try again.");
+//       }
+//     } catch (error) {
+//       console.error("⚠️ Fetch error:", error);
+//       alert("Error submitting form. Check console for more details.");
+//     }
+//   });
+// });

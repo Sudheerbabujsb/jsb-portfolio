e750a6c1f610498ac7c34f347aa9bf2a2ea4e80f
Jangam Sudheer Babu <sudheerbabujsb@gmail.com>
2026-01-11 22:45:04 +0530

    Notification

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,38 @@
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const { onValueCreated } =
+  require("firebase-functions/v2/database");
+
 /**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * Gen-2 RTDB trigger
  */
+exports.sendPushOnNewMessage = onValueCreated(
+  "/messages/{msgId}",
+  async (event) => {
+    const msg = event.data.val();
+    if (!msg || !msg.uid) return;
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+    const usersSnap =
+      await admin.database().ref("users").once("value");
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+    const tasks = [];
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+    usersSnap.forEach((user) => {
+      const u = user.val();
+      if (user.key !== msg.uid && u.fcmToken) {
+        tasks.push(
+          admin.messaging().send({
+            token: u.fcmToken,
+            notification: {
+              title: "New message 💚",
+              body: msg.text || "New message received"
+            }
+          })
+        );
+      }
+    });
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    return Promise.all(tasks);
+  }
+);
